fcba8a751a7ed77d7d9b50d23f63bba431bb5e60
Augustas <augis.seras@gmail.com>
2017-06-12 23:56:27 -0700

    The draft of the drafts

---
+++ PhotoApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoApp
+//
+//  Created by Pixelmator on 6/12/17.
+//  Copyright © 2017 Pixelmator. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Pixelmator on 6/12/17.
+//  Copyright © 2017 Pixelmator. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBOutlet var imageView: NSImageView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(imageView)
+        imageView?.image = NSImage(named: NSImage.Name(rawValue: "photo"))
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
